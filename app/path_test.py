a8c356d2319cff52d90239d9fa4e1bd13bee8cc5
jringenbach <ringenbach.julien@gmail.com>
2019-11-18 13:09:22 +0100

    Beginning of the templates

--- app/path_test.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.append('/Documents/Alternance/Projet/Pokedex/pokedex')
-sys.path.append('/Documents/Alternance/Projet/Pokedex/API')
-print(sys.path)
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 # coding: utf-8
 
+from flask import render_template
+from API.pokemonAPI import PokemonAPI
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    list_pokemon = PokemonAPI(params={"limit" : 5}).get_pokemon_from_api()
+    return render_template("index.html", title="Pokédex", list_pokemon=list_pokemon)
