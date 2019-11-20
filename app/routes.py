3a4e4e632aace0db971b098106432e3caf0ff044
jringenbach <ringenbach.julien@gmail.com>
2019-11-20 12:06:38 +0100

    Beginning of templates and pokedex

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,16 @@ from API.pokemonAPI import PokemonAPI
 from app import app
 
 @app.route('/')
-@app.route('/index')
 def index():
-    list_pokemon = PokemonAPI(params={"limit" : 5}).get_pokemon_from_api()
-    return render_template("index.html", title="Pokédex", list_pokemon=list_pokemon)
+    """Index du site web"""
+
+    return render_template("index.html", title="Accueil")
+
+
+
+@app.route('/pokedex')
+def pokedex():
+    """Page affichant le pokédex"""
+
+    list_pokemon = PokemonAPI(params={"limit" : 18}).get_pokemon_from_api()
+    return render_template("pokedex.html", title="Pokédex", list_pokemon=list_pokemon)
